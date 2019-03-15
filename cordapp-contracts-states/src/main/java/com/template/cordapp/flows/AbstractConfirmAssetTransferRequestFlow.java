18717e975b21d22d3770a81e7911fb953129c49b
Pooja Kulkarni <pk@w208-6j-v4.eduroam.dynamic.rbg.tum.de>
2019-03-15 17:52:38 +0100

    fix
    Signed-off-by: Pooja Kulkarni <pk@w208-6j-v4.eduroam.dynamic.rbg.tum.de>

--- cordapp-contracts-states/src/main/java/com/template/cordapp/flows/AbstractConfirmAssetTransferRequestFlow.java
+++ cordapp-contracts-states/src/main/java/com/template/cordapp/flows/AbstractConfirmAssetTransferRequestFlow.java
@@ -14,32 +14,33 @@ import org.jetbrains.annotations.NotNull;
 
 @InitiatingFlow
 @Metadata(
-   mv = {1, 1, 8},
-   bv = {1, 0, 2},
-   k = 1,
-   d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b'\u0018\u0000*\u0006\b\u0000\u0010\u0001 \u00012\b\u0012\u0004\u0012\u0002H\u00010\u00022\u00020\u0003B\u0005¢\u0006\u0002\u0010\u0004¨\u0006\u0005"},
-   d2 = {"Lcom/synechron/cordapp/flows/AbstractAssetSettlementFlow;", "T", "Lnet/corda/core/flows/FlowLogic;", "Lcom/synechron/cordapp/flows/FlowLogicCommonMethods;", "()V", "cordapp-contracts-states"}
+        mv = {1, 1, 8},
+        bv = {1, 0, 2},
+        k = 1,
+        d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b'\u0018\u0000*\u0006\b\u0000\u0010\u0001 \u00012\b\u0012\u0004\u0012\u0002H\u00010\u00022\u00020\u0003B\u0005¢\u0006\u0002\u0010\u0004¨\u0006\u0005"},
+        d2 = {"Lcom/synechron/cordapp/flows/AbstractAssetSettlementFlow;", "T", "Lnet/corda/core/flows/FlowLogic;", "Lcom/synechron/cordapp/flows/FlowLogicCommonMethods;", "()V", "cordapp-contracts-states"}
 )
-public abstract class AbstractAssetSettlementFlow extends FlowLogic implements FlowLogicCommonMethods {
-   @NotNull
-   public Party firstNotary(@NotNull ServiceHub $receiver) {
-      Intrinsics.checkParameterIsNotNull($receiver, "$receiver");
-      return DefaultImpls.firstNotary(this, $receiver);
-   }
+public abstract class AbstractConfirmAssetTransferRequestFlow extends FlowLogic implements FlowLogicCommonMethods {
+    @NotNull
+    public Party firstNotary(@NotNull ServiceHub $receiver) {
+        Intrinsics.checkParameterIsNotNull($receiver, "$receiver");
+        return DefaultImpls.firstNotary(this, $receiver);
+    }
 
-   @NotNull
-   public StateAndRef loadState(@NotNull ServiceHub $receiver, @NotNull UniqueIdentifier linearId, @NotNull Class clazz) {
-      Intrinsics.checkParameterIsNotNull($receiver, "$receiver");
-      Intrinsics.checkParameterIsNotNull(linearId, "linearId");
-      Intrinsics.checkParameterIsNotNull(clazz, "clazz");
-      return DefaultImpls.loadState(this, $receiver, linearId, clazz);
-   }
+    @NotNull
+    public StateAndRef loadState(@NotNull ServiceHub $receiver, @NotNull UniqueIdentifier linearId, @NotNull Class clazz) {
+        Intrinsics.checkParameterIsNotNull($receiver, "$receiver");
+        Intrinsics.checkParameterIsNotNull(linearId, "linearId");
+        Intrinsics.checkParameterIsNotNull(clazz, "clazz");
+        return DefaultImpls.loadState(this, $receiver, linearId, clazz);
+    }
 
-   @NotNull
-   public Party resolveIdentity(@NotNull ServiceHub $receiver, @NotNull AbstractParty abstractParty) {
-      Intrinsics.checkParameterIsNotNull($receiver, "$receiver");
-      Intrinsics.checkParameterIsNotNull(abstractParty, "abstractParty");
-      return DefaultImpls.resolveIdentity(this, $receiver, abstractParty);
-   }
+    @NotNull
+    public Party resolveIdentity(@NotNull ServiceHub $receiver, @NotNull AbstractParty abstractParty) {
+        Intrinsics.checkParameterIsNotNull($receiver, "$receiver");
+        Intrinsics.checkParameterIsNotNull(abstractParty, "abstractParty");
+        return DefaultImpls.resolveIdentity(this, $receiver, abstractParty);
+    }
 }
+
 
